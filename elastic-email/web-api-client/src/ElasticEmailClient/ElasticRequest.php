dec2a88bde19de442ccfc79bc772eb51db4d37d8
t-m-eight <truchan.michal@gmail.com>
2019-03-27 15:16:43 +0100

    add web api
    adding web api files

---
+++ elastic-email/web-api-client/src/ElasticEmailClient/ElasticRequest.php
@@ -0,0 +1,171 @@
+<?php
+	namespace ElasticEmailClient; 
+
+    abstract class ElasticRequest {
+        /**
+         * @var \ElasticEmailClient\ApiConfiguration ApiConfiguration
+         */
+        protected $configuration;
+
+        /**
+         * @var \GuzzleHttp\ClientInterface
+         */
+        private $httpClient;
+
+        public function __construct(\ElasticEmailClient\ApiConfiguration $conf)
+        {
+            $this->configuration = $conf;
+
+            if ($conf->getClientInterface()) {
+                $this->httpClient = $conf->getClientInterface();
+            } else {
+                $this->httpClient = new \GuzzleHttp\Client([
+                    'timeout'  => $this->configuration->getTimeout()
+                ]);
+            }
+        }
+
+        /**
+         * @param string $url
+         * @param array $data
+         * @param string $method
+         * @param array $attachments
+         * @return \Psr\Http\Message\ResponseInterface
+         * @throws \Exception
+         */
+        protected function sendRequest(string $url, array $data = [], string $method = 'POST', array $attachments = [])
+        {
+            $method = strtoupper($method);
+        
+            if (!in_array($method, ApiConfiguration::AVAILABLE_REQUEST_METHODS))
+            {
+                throw new \Exception('Unallowed request method type');
+            }
+
+            $options = [];
+            $data['apikey'] = $this->configuration->getApiKey();
+
+            if (!empty($attachments) && $method === 'POST') {
+                $options['multipart'] = $this->parseMultipart($attachments, $data);
+            } else {
+                if (!empty($data) && empty($attachments) && $method === 'POST') {
+                    $options['form_params'] = $data;
+                } else {
+                    $url.= '?'.http_build_query($data);
+                }
+            }
+
+            $url = $this->configuration->getApiUrl(). $url;
+        
+            try
+            {
+                $response = $this->httpClient->request($method, $url, $options);
+                
+                if(is_object($response) && !empty($response->getBody())){
+                $resp = json_decode($response->getBody()->getContents());
+                }else{
+                    $resp = '';
+                }
+                
+            } catch (\Exception $e) {
+                throw $e;
+            }
+            if(!is_wp_error($response)){
+                if($resp ->success == false)
+                
+                return "code Error:".$response->getBody();
+            }
+
+
+            if (isset($resp->data) && $resp->data) {
+                return $resp->data;
+            }
+             
+            return $resp;
+        }
+
+        /**
+            * @param array $attachments
+            * @param array $params
+            * @return array
+            */
+        private function parseMultipart(array $attachments, array $params): array
+        {
+            $result = [];
+
+            foreach ($attachments as $key => $attachment) {
+                $result[] = [
+                    'name' => 'file_'.$key,
+                    'contents' => fopen($attachment, 'r'),
+                    'filename' => basename($attachment)
+                ];
+            }
+
+            foreach ($params as $key => $param) {
+                $result[] = [
+                    'name' => $key,
+                    'contents' => $param
+                ];
+            }
+
+            return $result;
+        }
+
+        public function setHttpClient($client)
+        {
+            $this->httpClient = $client;
+            return $this;
+        }
+
+        /**
+        * @return \GuzzleHttp\ClientInterface
+        */
+        public function getHttpClient()
+        {
+            return $this->httpClient;
+        }
+
+        /**
+            * @param \ElasticEmailClient\ApiConfiguration $config
+            * @return $this
+            */
+        public function setConfiguration(\ElasticEmailClient\ApiConfiguration $config)
+        {
+            $this->configuration = $config;
+
+            return $this;
+        }
+
+        /**
+            * @return \ElasticEmailClient\ApiConfiguration
+            */
+        public function getConfiguration()
+        {
+            return $this->configuration;
+        }
+
+    }
+    class ApiException extends \Exception {
+
+        public $url;
+        public $method;
+        public $rawResponse;
+    
+        /**
+         * @param string $url
+         * @param string $method
+         * @param string $message
+         * @param string $rawResponse
+         */
+        public function __construct($url, $method, $message = "", $rawResponse = "") {
+            $this->url = $url;
+            $this->method = $method;
+            $this->rawResponse = $rawResponse;
+            parent::__construct($message);
+        }
+    
+        public function __toString() {
+            return strtoupper($this->method) . ' ' . $this->url . ' returned: ' . $this->getMessage();
+        }
+    
+    }
